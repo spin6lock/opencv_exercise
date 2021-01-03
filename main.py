59b0d892961b0a9e6b8f301051598f4c7caeb384
spin6lock <spin6lock@users.noreply.github.com>
2021-01-03 20:40:26 +0800

    add face recognition

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import cv2
 
+def face_recognition(frame):
+    face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 转为灰度图
+    # 检测脸部
+    faces = face_cascade.detectMultiScale(gray,
+                                          scaleFactor=1.1,
+                                          minNeighbors=5,
+                                          minSize=(30, 30),
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
+    print('Detected ', len(faces), " face")
+    return faces
+
+def mark_faces(img, faces):
+    # 标记位置
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    return img
+
 def main():
     # 捕获指定摄像头的实时视频流
     cap = cv2.VideoCapture(0)
@@ -13,6 +31,8 @@ def main():
         if not ret:
             print("图像获取失败")
             continue
+        faces = face_recognition(frame)
+        frame = mark_faces(frame, faces)
         cv2.imshow("image_win", frame)
         key = cv2.waitKey(100)
         if key == ord('q'):
